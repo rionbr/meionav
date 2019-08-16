74b45edae99df9280cb64520269fc562ce483e16
Rion Brattig Correia <rionbr@indiana.edu>
2019-08-16 10:13:14 +0100

    Screened 2019-08

--- 3-screened-data/merge_screening_data.py
+++ 3-screened-data/merge_screening_data.py
@@ -16,8 +16,8 @@ if __name__ == '__main__':
     df = pd.read_csv('../2-core_genes/results/DM/core_DM_meiotic_genes.csv').set_index('id_gene_DM')
     
     # Screened Data (From Experimental Analysis)
-    dfS = pd.read_csv('data/core_DM_screened.csv').set_index('id_gene_DM')
-
+    dfS = pd.read_csv('data/core_DM_screened_2019-08.csv').set_index('id_gene_DM')
+    print(dfS.head())
     cols1 = [
         'StockNumber', 'FlyBase Genotype'
     ]
@@ -25,11 +25,11 @@ if __name__ == '__main__':
         'FT1 eggs', 'FT1 hatched', 'FT2 eggs', 'FT2 hatched', 'FT3 eggs', 'FT3 hatched', 'FT4 eggs', 'FT4 hatched',
     ]
     cols3 = [
-        'Phenotype or literature', 'Previously known to affect spermatogenesis', 'Function', 'Overlapping', 'Previous ref to RNAi working'
+        'Phenotype or literature', 'Previously known to affect spermatogenesis', 'Function', 'Previous ref to RNAi working', 'Other species infertility phenotype?'
     ]
-    #df[cols1] = dfS[cols1]
+    df[cols1] = dfS[cols1]
     df[cols2] = dfS[cols2]
-    #df[cols3] = dfS[cols3]
+    df[cols3] = dfS[cols3]
 
     # Export
     df.to_csv('results/core_DM_meiotic_genes_screened.csv')
