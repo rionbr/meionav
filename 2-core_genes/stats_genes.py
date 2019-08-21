f5f8486ad3d9a9d082e459af0fb32829c549869b
Rion Brattig Correia <rionbr@indiana.edu>
2019-08-21 16:34:38 +0100

    Update to stats genes

--- 2-core_genes/stats_genes.py
+++ 2-core_genes/stats_genes.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     #
     # - number of genes
     #
-    print('--- Number of genes ---\n')
+    print('# Number of genes\n')
 
     # HS     
     df_HS_DGE_CyteGonia = pd.read_csv('../1-diff-gene-exp/results/HS/HS-DGE_Cyte_vs_Gonia.csv', index_col=0)
@@ -28,22 +28,22 @@ if __name__ == '__main__':
     df_HS_DGE_CyteGonia.index = df_HS_DGE_CyteGonia.index.map(lambda x: x.split('.')[0])
     df_HS_DGE_CyteTid.index = df_HS_DGE_CyteTid.index.map(lambda x: x.split('.')[0])
 
-    print('HS Cyte vs Gonia: {:,d}'.format(np.unique(df_HS_DGE_CyteGonia.index).shape[0]))
-    print('HS Cyte vs Tid: {:,d}\n'.format(np.unique(df_HS_DGE_CyteTid.index).shape[0]))
+    print('- HS Cyte vs Gonia: {:,d}'.format(np.unique(df_HS_DGE_CyteGonia.index).shape[0]))
+    print('- HS Cyte vs Tid: {:,d}\n'.format(np.unique(df_HS_DGE_CyteTid.index).shape[0]))
 
     # MM
     df_MM_DGE_CyteGonia = pd.read_csv('../1-diff-gene-exp/results/MM/MM-DGE_Cyte_vs_Gonia.csv', index_col=0)
     df_MM_DGE_CyteTid = pd.read_csv('../1-diff-gene-exp/results/MM/MM-DGE_Cyte_vs_Tid.csv', index_col=0)
 
-    print('MM Cyte vs Gonia: {:,d}'.format(np.unique(df_MM_DGE_CyteGonia.index).shape[0]))
-    print('MM Cyte vs Tid: {:,d}\n'.format(np.unique(df_MM_DGE_CyteTid.index).shape[0]))
+    print('- MM Cyte vs Gonia: {:,d}'.format(np.unique(df_MM_DGE_CyteGonia.index).shape[0]))
+    print('- MM Cyte vs Tid: {:,d}\n'.format(np.unique(df_MM_DGE_CyteTid.index).shape[0]))
     
     # DM
     df_DM_DGE_Apical = pd.read_csv('../1-diff-gene-exp/results/DM/DM_DGE_ApicalTestis.csv', index_col=0)
     df_DM_DGE_MidTestis = pd.read_csv('../1-diff-gene-exp/results/DM/DM_DGE_MidTestis.csv', index_col=0)
 
-    print('DM Apical: {:,d}'.format(np.unique(df_DM_DGE_Apical.index).shape[0]))
-    print('DM MidTestis: {:,d}\n'.format(np.unique(df_DM_DGE_MidTestis.index).shape[0]))
+    print('- DM Apical: {:,d}'.format(np.unique(df_DM_DGE_Apical.index).shape[0]))
+    print('- DM MidTestis: {:,d}\n'.format(np.unique(df_DM_DGE_MidTestis.index).shape[0]))
 
     del df_HS_DGE_CyteGonia, df_HS_DGE_CyteTid,
     del df_MM_DGE_CyteGonia, df_MM_DGE_CyteTid,
@@ -51,7 +51,7 @@ if __name__ == '__main__':
     #
     # - number of genes differently expressed
     #
-    print("--- Number of genes differently expressed ---\n")
+    print("# Number of genes differently expressed\n")
     
     # HS     
     df_HS_DE_UpCyteGonia01 = pd.read_csv('../1-diff-gene-exp/results/HS/DE/HS_DE_UpCyte_vs_Gonia-FDR_0p01.csv', index_col=0)
@@ -59,10 +59,10 @@ if __name__ == '__main__':
     df_HS_DE_DownCyteTid01 = pd.read_csv('../1-diff-gene-exp/results/HS/DE/HS_DE_DownCyte_vs_Tid-FDR_0p01.csv', index_col=0)
     df_HS_DE_DownCyteTid05 = pd.read_csv('../1-diff-gene-exp/results/HS/DE/HS_DE_DownCyte_vs_Tid-FDR_0p05.csv', index_col=0)
 
-    print('MM (Up)Cyte vs Gonia: FDR=0.01: {:,d}; FDR=0.05: {:,d}'.format(
+    print('- MM (Up)Cyte vs Gonia: FDR=0.01: {:,d}; FDR=0.05: {:,d}'.format(
         np.unique(df_HS_DE_UpCyteGonia01.index).shape[0],
         np.unique(df_HS_DE_UpCyteGonia05.index).shape[0]))
-    print('MM (Down)Cyte vs Tid: FDR=0.01: {:,d}; FDR=0.05: {:,d}\n'.format(
+    print('- MM (Down)Cyte vs Tid: FDR=0.01: {:,d}; FDR=0.05: {:,d}\n'.format(
         np.unique(df_HS_DE_DownCyteTid01.index).shape[0],
         np.unique(df_HS_DE_DownCyteTid05.index).shape[0]))
 
@@ -72,10 +72,10 @@ if __name__ == '__main__':
     df_MM_DE_DownCyteTid01 = pd.read_csv('../1-diff-gene-exp/results/MM/DE/MM_DE_DownCyte_vs_Tid-FDR_0p01.csv', index_col=0)
     df_MM_DE_DownCyteTid05 = pd.read_csv('../1-diff-gene-exp/results/MM/DE/MM_DE_DownCyte_vs_Tid-FDR_0p05.csv', index_col=0)
 
-    print('MM (Up)Cyte vs Gonia: FDR=0.01: {:,d}; FDR=0.05: {:,d}'.format(
+    print('- MM (Up)Cyte vs Gonia: FDR=0.01: {:,d}; FDR=0.05: {:,d}'.format(
         np.unique(df_MM_DE_UpCyteGonia01.index).shape[0],
         np.unique(df_MM_DE_UpCyteGonia05.index).shape[0]))
-    print('MM (Down)Cyte vs Tid: FDR=0.01: {:,d}; FDR=0.05: {:,d}\n'.format(
+    print('- MM (Down)Cyte vs Tid: FDR=0.01: {:,d}; FDR=0.05: {:,d}\n'.format(
         np.unique(df_MM_DE_DownCyteTid01.index).shape[0],
         np.unique(df_MM_DE_DownCyteTid05.index).shape[0]))
 
@@ -83,8 +83,8 @@ if __name__ == '__main__':
     df_DM_DE_UpApical = pd.read_csv('../1-diff-gene-exp/results/DM/DE/DM_DE_UpApicalTestis.csv', index_col=0)
     df_DM_DE_DownMid = pd.read_csv('../1-diff-gene-exp/results/DM/DE/DM_DE_DownMidTestis.csv', index_col=0)
 
-    print('DM (Up)Apical Testis: {:,d}'.format(np.unique(df_DM_DE_UpApical.index).shape[0]))
-    print('DM (Down)MidTestis: {:,d}\n'.format(np.unique(df_DM_DE_DownMid.index).shape[0]))
+    print('- DM (Up)Apical Testis: {:,d}'.format(np.unique(df_DM_DE_UpApical.index).shape[0]))
+    print('- DM (Down)MidTestis: {:,d}\n'.format(np.unique(df_DM_DE_DownMid.index).shape[0]))
     
     del df_HS_DE_UpCyteGonia01, df_HS_DE_UpCyteGonia05, df_HS_DE_DownCyteTid01, df_HS_DE_DownCyteTid05
     del df_MM_DE_UpCyteGonia01, df_MM_DE_UpCyteGonia05, df_MM_DE_DownCyteTid01, df_MM_DE_DownCyteTid05
@@ -97,24 +97,24 @@ if __name__ == '__main__':
     #
     # - number of protein-coding genes differently expressed
     #
-    print('--- Number of Protein-Coding genes ---\n')
+    print('# Number of Protein-Coding genes\n')
 
     # HS
     df_HS_genes01 = pd.read_csv('results/HS/genes_HS-FDR_0p01.csv', index_col=0)
     df_HS_genes05 = pd.read_csv('results/HS/genes_HS-FDR_0p05.csv', index_col=0)
 
-    print('HS: FDR=0.01:\n')
+    print('## HS with FDR=0.01:\n')
     print(df_HS_genes01['biotype'].value_counts().to_string())
-    print('\nHS: FDR=0.05:\n')
+    print('\n## HS with: FDR=0.05:\n')
     print(df_HS_genes05['biotype'].value_counts().to_string())
 
     # MM
     df_MM_genes01 = pd.read_csv('results/MM/genes_MM-FDR_0p01.csv', index_col=0)
     df_MM_genes05 = pd.read_csv('results/MM/genes_MM-FDR_0p05.csv', index_col=0)
 
-    print('\nMM: FDR=0.01:\n')
+    print('\nMM with FDR=0.01:\n')
     print(df_MM_genes01['biotype'].value_counts().to_string())
-    print('\nMM: FDR=0.05:\n')
+    print('\nMM with FDR=0.05:\n')
     print(df_MM_genes05['biotype'].value_counts().to_string())
 
     # DS
